c61082c86477b262489e9ca8eb538b8634459cf6
HomaX1 <baitasova.anastasiia@gmail.com>
2018-01-14 04:16:02 +0200

    reservation table was done

--- js/main/modules/reservation.js
+++ js/main/modules/reservation.js
@@ -9,7 +9,8 @@ var schemeTables = '.scheme-tables',
     validPhone = '#valid-phone',
     validTableNumber = '#valid-table-number',
     formInput = '.form-reservation__input',
-    successSubmit = '.form-reservation__text';
+    successSubmit = '.form-reservation__text',
+    tablesItem = '.scheme-tables__item';
 
 var reservationFunctions = {
     showFormList: function () {
@@ -24,26 +25,6 @@ var reservationFunctions = {
             }
 
         });
-    },
-    selectOptionList: function () {
-
-        $(formSelectList).click(function (e) {
-            var optionValue = '';
-
-            if ($(e.target.parentElement).hasClass('form-select-list-from')) {
-                $(successSubmit).hide();
-                $(formSelectList).fadeOut();
-                optionValue = e.target.innerText;
-                $('.form-select__text-from').text(optionValue);
-                $('.form-select-from').removeClass('border');
-            } else {
-                $(successSubmit).hide();
-                $(formSelectList).fadeOut();
-                optionValue = e.target.innerText;
-                $('.form-select__text-to').text(optionValue);
-                $('.form-select-to').removeClass('border');
-            }
-        });
 
         $(document).mouseup(function (e) {
             if ($(formSelect).has(e.target).length === 0) {
@@ -51,19 +32,97 @@ var reservationFunctions = {
             }
         });
     },
-    numberTransfer: function () {
-        $(schemeTables).on('click', function (e) {
-            var dataIdNum = +$(e.target).attr('data-id');
+    selectOptionList: function () {
 
-            if (typeof dataIdNum === 'number') {
-                $(tableNumberInput).val(dataIdNum);
+        $(formSelectList).click(function (e) {
+            var optionValue = '';
+
+            if(!$('.form-select-list li').hasClass('disabled-time')) {
+                if ($(e.target.parentElement).hasClass('form-select-list-from')) {
+                    $(successSubmit).hide();
+                    $(formSelectList).fadeOut();
+                    optionValue = e.target.innerText;
+                    $('.form-select__text-from').text(optionValue);
+                    $('.form-select-from').removeClass('border');
+                } else {
+                    $(successSubmit).hide();
+                    $(formSelectList).fadeOut();
+                    optionValue = e.target.innerText;
+                    $('.form-select__text-to').text(optionValue);
+                    $('.form-select-to').removeClass('border');
+                }
             }
 
+        });
+    },
+    numberTransfer: function () {
+        $(schemeTables).on('click', function (e) {
             $(successSubmit).hide();
             $(validTableNumber).hide();
             $(tableNumberInput).removeClass('border');
 
-        })
+            if ($(e.target).hasClass('scheme-tables__item') && !$(e.target).hasClass('booked-table')) {
+                var dataIdNum = +$(e.target).attr('data-id');
+                $(tableNumberInput).val(dataIdNum);
+            }
+
+            if ($(e.target).attr('data-times').length > 0) {
+                var timesArray = JSON.parse($(e.target).attr('data-times'));
+
+                $('.form-select-list__item').removeClass('disabled-time');
+
+                $('.form-select-list-from li').each(function(index, item) {
+                    var itemFrom = parseInt($(item).text());
+
+                    timesArray.forEach(function (timesArrayElem) {
+                            for (var i = timesArrayElem.timeFrom; i < timesArrayElem.timeTo; i++) {
+                                if(itemFrom === i) {
+                                    $(item).addClass('disabled-time');
+                                }
+                            }
+                    });
+                });
+
+                $('.form-select-list-to li').each(function(index, item) {
+                    var itemTo = parseInt($(item).text());
+
+                    timesArray.forEach(function (timesArrayElem) {
+
+                        for (var i = timesArrayElem.timeFrom + 1; i <= timesArrayElem.timeTo; i++) {
+                            if(itemTo === i) {
+                                $(item).addClass('disabled-time');
+                            }
+                        }
+
+                    });
+                });
+
+
+                /*timesArray.forEach(function (timesArrayElem) {
+                    console.log(timesArrayElem);
+
+                    for (var i = timesArrayElem.timeFrom; i <= timesArrayElem.timeTo; i++) {
+                        fromArray.push(i);
+                        toArray.push(i);
+
+                    }
+
+                });
+
+                console.log(fromArray, 'fromArray');
+                console.log(toArray, 'toArray');*/
+
+                /*$(timesArray).each(function (index, timesArrayItem) {
+                    console.log(timesArrayItem);
+
+                     if(timesArrayItem) {
+
+                     }
+                })*/
+            }
+
+
+        });
     },
     validation: function () {
         $(formInput).keyup(function (e) {
@@ -75,6 +134,7 @@ var reservationFunctions = {
                 case 'first-name':
                     if ($(this).val() !== '') {
                         var patternName = /^[A-Za-z]{1,50}$/;
+
                         if (!patternName.test($(this).val())) {
                             $(this).addClass('border');
                             $(validName).text('You can use latin letters only!').show();
@@ -87,6 +147,7 @@ var reservationFunctions = {
                 case 'phone-number':
                     if ($(this).val() !== '') {
                         var patternPhone = /^\d{3}[\-]\d{3}[\-]\d{4}$/;
+
                         if (!patternPhone.test($(this).val())) {
                             $(this).addClass('border');
                             $(validPhone).text('Enter the phone number as in the example!').show();
@@ -97,6 +158,38 @@ var reservationFunctions = {
                     }
                     break;
             }
+        });
+    },
+    getBookedTables: function () {
+        $.get('/tables.json', function (response) {
+
+            $(tablesItem).each(function (index, tableItem) {
+
+
+                var bookedTables = response.filter(function (item) {
+                    return item.tablesNumber === +$(tableItem).attr('data-id');
+                });
+
+                if (bookedTables[0].booking !== false && bookedTables[0].booking.length !== 0) {
+
+                    /*bookedTables[0].booking.forEach(function (bookingElem) {
+
+                        for (var i = bookingElem.timeFrom; i <= bookingElem.timeTo; i++) {
+                            fromArray.push(i);
+                            toArray.push(i);
+                        }
+                        console.log(fromArray, 'fromArray');
+                        console.log(toArray, 'toArray');
+                    });
+                    fromArray.pop();
+                    toArray.shift();*/
+
+
+                    $(tableItem).attr('data-times', JSON.stringify(bookedTables[0].booking));
+                } else {
+                    $(tableItem).addClass('booked-table');
+                }
+            });
         });
     },
     sendData: function () {
@@ -126,6 +219,7 @@ var reservationFunctions = {
         })
     },
     renderReservation: function () {
+        reservationFunctions.getBookedTables();
         reservationFunctions.showFormList();
         reservationFunctions.selectOptionList();
         reservationFunctions.numberTransfer();
